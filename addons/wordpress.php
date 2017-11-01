d9e77e3ff77b61401c18c3104ca5d80281bdfcc0
Mateo Balcorta <misterrattail@gmail.com>
2017-11-01 10:11:28 -0700

    Remove getFields function and move to WordPressDb class.

--- addons/wordpress.php
+++ addons/wordpress.php
@@ -1,21 +1,53 @@
 <?
+require_once(__DIR__. '/wordpressdb.php');
+
+
 class WordPress {
-  function getOptions($param)
+
+function getOptions($param)
   {
     return get_option($param);
   }
 
-  function wordPressQuery()
+function wordPressQuery()
   {
     return new WP_Query(array(
         'post_type' => 'campaign',
         'post_status' => 'publish',
     ));
   }
-  function getFields($id){
-    return get_fields($id);
-  }
 
+
+
+function loopActiveCampaigns($results)
+ {
+   global $wpdb;
+
+    $campaigns = array();
+    foreach ($results->posts as $campaign) {
+        $id = $campaign->ID;
+        $campaigns[$id] = array(
+            'conversions' => 0,
+            'fields' => $wpdb->getFields($id),
+            'id' => $id,
+            'losses' => 0,
+            'sent' => 0,
+            'subjects' => array(),
+            'title' => $campaign->post_title,
+            'valid' => true,
+        );
+        // Get all subjects
+        $subjects = $campaigns[$id]['fields']['subjects'];
+        for ($i = 0; $i < count($subjects); $i++) {
+            $campaigns[$id]['subjects'][$i] = array(
+                'conversions' => 0,
+                'losses' => 0,
+                'sent' => 0,
+            );
+         }
+      }
+     return $campaigns;
+  }
 }
 
 function wp() {
