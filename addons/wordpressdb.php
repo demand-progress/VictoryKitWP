8a41d4834e29adae03ba6ba7a6e65bd3a45002e8
Mateo Balcorta <misterrattail@gmail.com>
2017-11-02 11:42:17 -0700

    Add getReuslts method to WordPressDb class.

--- addons/wordpressdb.php
+++ addons/wordpressdb.php
@@ -1,8 +1,21 @@
 <?
 class WordPressDb {
-  function get_results(){
-    return array('campaign_id'=>0);
+
+  function getResults($wpdb){
+    return $wpdb->get_results('
+       SELECT
+           vkm.campaign_id,
+           vkm.variation_subject,
+           SUM(vkm.conversions) as conversions,
+           SUM(vkm.losses) as losses,
+           SUM(vkm.sent) as sent
+       FROM
+           vk_mailing AS vkm
+       GROUP BY
+           vkm.campaign_id, vkm.variation_subject
+    ', ARRAY_A);
   }
+
   function getFields($id){
     return get_fields($id);
   }
