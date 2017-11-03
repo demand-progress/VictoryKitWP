cc5fb4a4a3b438b818c79e5476a0741f17a24aef
Mateo Balcorta <misterrattail@gmail.com>
2017-11-03 09:34:14 -0700

    Add boost function test.

--- addons/tests/mailing_test.php
+++ addons/tests/mailing_test.php
@@ -117,6 +117,15 @@ final class RequestMethodTest extends TestCase
                                 , $result);
     }
 
+    public function testBoostFunction()
+    {
+      $overall = Array('conversions'=> 10, 'losses' => 10, 'sent' => 10);
+      $boost = 500;
+      $wp = new WordPress();
+      $result = $wp->boost($overall, $boost);
+      $this->assertSame(array('boost_value' => 500, 'overall_value' => array('conversions' => 10, 'losses' => 10, 'sent' => 10, 'boost' => 500, 'rate' => 0.9803921568627451)), $result);
+    }
+
     public function testGetMailingsStatsFromAkQueryMethod()
     {
       global $ak;
