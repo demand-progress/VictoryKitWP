57ee466775b87147965d1e4fb99bfdd113a2da55
Mateo Balcorta <misterrattail@gmail.com>
2017-11-06 10:29:11 -0800

    Add Helpers class and loopActiveCampaigns function.

---
+++ addons/helperFunctions.php
@@ -0,0 +1,44 @@
+<?
+
+class Helpers {
+  function loopActiveCampaigns($results, $wpdb)
+   {
+      $campaigns = array();
+      foreach ($results->posts as $campaign) {
+          $id = $campaign->ID;
+          $campaigns[$id] = array(
+              'conversions' => 0,
+              'fields' => $wpdb->getFields($id),
+              'id' => $id,
+              'losses' => 0,
+              'sent' => 0,
+              'subjects' => array(),
+              'title' => $campaign->post_title,
+              'valid' => true,
+          );
+          // Get all subjects
+          $subjects = $campaigns[$id]['fields']['subjects'];
+          for ($i = 0; $i < count($subjects); $i++) {
+              $campaigns[$id]['subjects'][$i] = array(
+                  'conversions' => 0,
+                  'losses' => 0,
+                  'sent' => 0,
+              );
+           }
+        }
+       return $campaigns;
+    }
+}
+
+function hp() {
+    global $hp;
+
+    if(!isset($hp)) {
+        $hp = new Helpers();
+    }
+
+    return $hp;
+}
+
+// Initialize
+hp();
