9cd32f6d5cf6be9c6419e400cde07fad70289371
Mateo Balcorta <misterrattail@gmail.com>
2017-11-06 15:57:08 -0800

    Add overall_rate_calucation_with_boost function to mailingsHelper class.

--- addons/mailingsHelpers.php
+++ addons/mailingsHelpers.php
@@ -61,21 +61,85 @@ class MailingsHelpers {
        return $campaigns;
     }
 
-    function overall_rate_calculation_with_boost($overall, $boost){
-      $results = array();
-      $overall['boost'] = $boost;
+  function overall_rate_calculation_with_boost($overall, $boost){
+    $results = array();
+    $overall['boost'] = $boost;
 
-      // Overall rate
-      $overall['rate'] = ($overall['conversions'] - $overall['losses'] + $boost) / ($overall['sent'] + $boost);
-      if ($overall['rate'] < 0) {
-          $overall['rate'] = 0; // TODO: why would we not track negative results?
-      }
-
-       $results['boost_value'] = $boost;
-       $results['overall_value'] = $overall;
-
-       return $results;
+    // Overall rate
+    $overall['rate'] = ($overall['conversions'] - $overall['losses'] + $boost) / ($overall['sent'] + $boost);
+    if ($overall['rate'] < 0) {
+        $overall['rate'] = 0; // TODO: why would we not track negative results?
     }
+
+     $results['boost_value'] = $boost;
+     $results['overall_value'] = $overall;
+
+     return $results;
+  }
+
+  function calculate_shares($campaigns, $overall, $boost){
+    $results = array();
+    $campaign_rate_sum = 0;
+    foreach ($campaigns as $campaign_index => &$campaign) {
+        // Subjects
+        $fields = $campaign['fields'];
+        $subject_rate_sum = 0;
+        $valid_subjects = 0;
+
+        foreach ($campaign['subjects'] as $subject_index => &$subject) {
+            $enabled = $fields['subjects'][$subject_index]['enabled'];
+            $subject['title'] = $fields['subjects'][$subject_index]['subject'];
+            if (!$enabled) {
+                $subject['rate'] = 0;
+                continue;
+            }
+
+            $valid_subjects++;
+
+            $rate = (
+                ($subject['conversions'] - $subject['losses'] + $boost * $overall['rate'])
+                /
+                ($subject['sent'] + $boost)
+            );
+
+            if ($rate < 0) {
+                $subject['rate'] = 0; // TODO: why not track negative rates?
+                continue;
+            }
+
+            $subject['rate'] = $rate;
+            $subject_rate_sum += $rate; // TODO: why not track negative rates?
+        }
+
+        // If no enabled subjects skip this campaign
+        if ($valid_subjects == 0) {
+            $campaign['valid'] = false;
+            continue;
+        }
+
+        foreach ($campaign['subjects'] as $subject_index => &$subject) {
+            $share = $subject_rate_sum ? $subject['rate'] / $subject_rate_sum : 0;
+            $subject['share'] = $share;
+        }
+
+        // Campaign
+        $rate = (
+            ($campaign['conversions'] - $campaign['losses'] + $boost * $overall['rate'])
+            /
+            ($campaign['sent'] + $boost)
+        );
+
+        if ($rate < 0) {
+            $campaign['rate'] = 0; // TODO: why not track negative rates?
+            continue;
+        }
+
+        $campaign['rate'] = $rate;
+        $campaign_rate_sum += $rate;
+    }
+     $results['campaigns_values'] = $campaigns;
+     return $results;
+  }
 }
 
 function mh() {
