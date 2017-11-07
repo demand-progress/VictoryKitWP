f49f86e8322ed1e9615687bcd457f7bfe2279ee2
Mateo Balcorta <misterrattail@gmail.com>
2017-11-06 16:12:38 -0800

    Add query, get_col and update functions to WordPress class.

--- addons/wordpress.php
+++ addons/wordpress.php
@@ -3,19 +3,21 @@ require_once(__DIR__. '/wordpressdb.php');
 
 
 class WordPress {
+  function query(){}
+  function get_col(){}
+  function update(){}
+  function getOptions($param)
+    {
+      return get_option($param);
+    }
 
-function getOptions($param)
-  {
-    return get_option($param);
-  }
-
-function wordPressQuery()
-  {
-    return new WP_Query(array(
-        'post_type' => 'campaign',
-        'post_status' => 'publish',
-    ));
-  }
+  function wordPressQuery()
+    {
+      return new WP_Query(array(
+          'post_type' => 'campaign',
+          'post_status' => 'publish',
+      ));
+    }
 }
 
 function wp() {
