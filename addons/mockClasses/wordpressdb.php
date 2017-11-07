d6002f2ff514084d8578d0a925923e1e16418304
Mateo Balcorta <misterrattail@gmail.com>
2017-11-06 20:43:35 -0800

    Remove getResults function form workdPressDb  class.

--- addons/mockClasses/wordpressdb.php
+++ addons/mockClasses/wordpressdb.php
@@ -1,20 +1,7 @@
 <?
 class WordPressDb {
 
-  function getResults($wpdb){
-    return $wpdb->get_results('
-       SELECT
-           vkm.campaign_id,
-           vkm.variation_subject,
-           SUM(vkm.conversions) as conversions,
-           SUM(vkm.losses) as losses,
-           SUM(vkm.sent) as sent
-       FROM
-           vk_mailing AS vkm
-       GROUP BY
-           vkm.campaign_id, vkm.variation_subject
-    ', ARRAY_A);
-  }
+
 
   function getFields($id){
     return get_fields($id);
