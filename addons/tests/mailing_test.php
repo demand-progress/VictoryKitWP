f7490045f4a7da15b490dcc33b440d2131cb62ac
Mateo Balcorta <misterrattail@gmail.com>
2018-06-21 13:57:44 -0700

    Added functions and variables to complete get_distributions function.

--- addons/tests/mailing_test.php
+++ addons/tests/mailing_test.php
@@ -1,13 +1,29 @@
 <?
 declare(strict_types=1);
 define('ABSPATH', 1);
+define('ARRAY_A', 1);
 
 require_once(__DIR__. '/../actionkit.php');
 require_once(__DIR__. '/../mailings.php');
 require_once(__DIR__. '/arrayCampaigns.php');
 
-function get_option(){}
+function get_option(){
+    return true;
+}
 function add_action(){}
+function get_permalink(){}
+
+class WP_Query {
+    public $posts = array(); 
+}
+
+class wpdb {
+    function get_results(){
+        return array();
+    }
+};
+
+$wpdb = new wpdb;
 
 use PHPUnit\Framework\TestCase;
 
@@ -15,17 +31,22 @@ final class mailingsClass extends TestCase
     {
         public function test_vk_mailings_create_new_mailings_action(): void
             {   
-                global $vk_mailings, $wpdb, $CampaignArray;
-                $vk_mailings = $this->createMock(Mailings::class);
-                $vk_mailings->expects($this->once())
+                global $CampaignArray;
+
+                $vk_mailings_mock = $this->createMock(Mailings::class);
+                $vk_mailings_mock->expects($this->once())
                          ->method('get_distributions')
                          ->willReturn($CampaignArray);
                        
-                $vk_mailings->expects($this->once())
-                        ->method('send');
-                
-                    $result = vk_mailings_create_new_mailings_action();
+                // $vk_mailings->expects($this->exactly(2))
+                //           ->method('get_fresh_subscribers_for_campaign')
+                //           ->willReturn(array( 0 => 6630470, 1 => 6630472,));
+
+                // $vk_mailings->expects($this->exactly(2))
+                //             ->method('send');
+    
+                $result = vk_mailings_create_new_mailings_action();
    
-                $this->assertTrue($result === array ( 'campaigns' => array ( ), 'overall' => array ( ), ));
+                $this->assertFalse($result === false);
             }
     }
