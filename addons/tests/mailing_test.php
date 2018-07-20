873b235a1b669eec3931141a4042a11ac8848ea7
Mateo Balcorta <misterrattail@gmail.com>
2018-07-20 11:29:37 -0700

    Added test for single campaign.

--- addons/tests/mailing_test.php
+++ addons/tests/mailing_test.php
@@ -5,10 +5,11 @@ define('ARRAY_A', 1);
 
 require_once(__DIR__. '/../actionkit.php');
 require_once(__DIR__. '/../mailings.php');
-require_once(__DIR__. '/arrayCampaigns.php');
+require_once(__DIR__. '/twoCampaigns.php');
+require_once(__DIR__. '/singleCampaign.php');
 
 function get_option(){
-    return true;
+    return 25;
 }
 function add_action(){}
 function get_permalink(){}
@@ -21,6 +22,12 @@ class wpdb {
     function get_results(){
         return array();
     }
+
+    function get_col(){
+        return array();
+    }
+    
+    function query(){}
 };
 
 $wpdb = new wpdb;
@@ -31,22 +38,45 @@ final class mailingsClass extends TestCase
     {
         public function test_vk_mailings_create_new_mailings_action(): void
             {   
-                global $CampaignArray;
+                global $TwoCampaigns;
 
                 $vk_mailings_mock = $this->createMock(Mailings::class);
+            
                 $vk_mailings_mock->expects($this->once())
-                         ->method('get_distributions')
-                         ->willReturn($CampaignArray);
+                        ->method('get_distributions')
+                        ->willReturn($TwoCampaigns);
                        
-                // $vk_mailings->expects($this->exactly(2))
-                //           ->method('get_fresh_subscribers_for_campaign')
-                //           ->willReturn(array( 0 => 6630470, 1 => 6630472,));
+                $vk_mailings_mock->expects($this->exactly(2))
+                        ->method('get_fresh_subscribers_for_campaign')
+                        ->willReturn(array( 0 => 6630470, 1 => 6630472,));
 
-                // $vk_mailings->expects($this->exactly(2))
-                //             ->method('send');
-    
+                $vk_mailings_mock->expects($this->exactly(3))
+                        ->method('send');
+
                 $result = vk_mailings_create_new_mailings_action($vk_mailings_mock);
-   
-                $this->assertFalse($result === false);
+                
+                // $this->assertTrue($result === null);
             }
+
+        public function test_get_distribution_one_campaign(): void
+        {   
+            global $singleCampaign;
+
+            $vk_mailings_mock = $this->createMock(Mailings::class);
+        
+            $vk_mailings_mock->expects($this->once())
+                    ->method('get_distributions')
+                    ->willReturn($singleCampaign);
+                   
+            $vk_mailings_mock->expects($this->exactly(1))
+                    ->method('get_fresh_subscribers_for_campaign')
+                    ->willReturn(array( 0 => 6630470, 1 => 6630472,));
+
+            $vk_mailings_mock->expects($this->exactly(2))
+                    ->method('send');
+
+            vk_mailings_create_new_mailings_action($vk_mailings_mock);
+            
+            // $this->assertFalse($result === false);
+        }
     }
